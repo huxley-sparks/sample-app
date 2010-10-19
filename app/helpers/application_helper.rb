4bbabc3da803b0ea774b31e611de22e80405b65e
Huxley <huxley.spark@gmail.com>
2010-10-19 09:32:49 -0700

    ch5 exercises

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,9 @@ module ApplicationHelper
 			"#{base_title} | #{@title}"
 		end
 	end
+
+	# create link for logo
+	def logo
+		logo = image_tag("logo.png", :alt => "Sample App", :class => "round")
+	end
 end
