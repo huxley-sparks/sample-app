83fb0f546089fd94c50db50d2cf2f60f54850f40
Huxley <huxley.spark@gmail.com>
2010-10-20 08:13:24 -0700

    spork reset, ch8 exercises

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,9 @@ class UsersController < ApplicationController
 		  redirect_to @user
 	  else
 		  @title = "Sign up"
+		  # reset passwords when errors display
+		  @user.password = ''
+		  @user.password_confirmation = ''
 		  render 'new'
 	  end
   end
