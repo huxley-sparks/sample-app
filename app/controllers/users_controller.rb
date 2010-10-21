a2e1313a2d6b5533c4239957e3badfcc8789f58d
Huxley <huxley.spark@gmail.com>
2010-10-21 13:27:46 -0700

    'model_name' error => problem in loop, '@'

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 	# before creating, do the following
 	before_filter :authenticate, :only => [:index, :edit, :update]
 	before_filter :correct_user, :only => [:edit, :update]
+	before_filter :admin_user,	 :only => :destroy
 
   def new
 	  @user = User.new
@@ -52,6 +53,12 @@ class UsersController < ApplicationController
 	  @title = @user.name
   end
 
+  def destroy
+	  User.find(params[:id]).destroy
+	  flash[:success] = "User destroyed."
+	  redirect_to users_path
+  end
+
   private
 
   	def authenticate
@@ -61,5 +68,9 @@ class UsersController < ApplicationController
 	def correct_user
 		@user = User.find(params[:id])
 		redirect_to(root_path) unless current_user?(@user)
+	end
+
+	def admin_user
+		redirect_to(root_path) unless current_user.admin?
 	end
 end
