cc66f07ae07597199ede5652a4092cc9b7787653
Huxley <huxley.spark@gmail.com>
2010-10-25 18:12:45 -0700

    un/follow button, user follows

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ SampleApp::Application.routes.draw do
 
   resources :sessions, :only => [:new, :create, :destroy]
   resources :microposts,	:only => [:create, :destroy]
+  resources :relationships, :only => [:create, :destroy]
 
   # each match creates, using signup as an example
   # signup_path and signup_url
