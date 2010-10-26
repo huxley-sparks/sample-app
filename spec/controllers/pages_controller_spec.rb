e73997029110e5087a221989f752d00313a2c21f
Huxley <huxley.spark@gmail.com>
2010-10-25 17:37:16 -0700

    followed/following links on homepage, using stats

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -25,7 +25,7 @@ describe PagesController do
 
 		  it "should have the right title" do
 			  response.should have_selector("title",
-											:content => "#{base_title} | Home")
+											:content => "#{@base_title} | Home")
 		  end
 	  end
 
